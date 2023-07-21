64315776997fe2c3cd7b94af983beb90a682f4c7
OUSSAMA SEFIANI <oussama.sefiani@um5r.ac.ma>
2023-07-21 14:55:23 +0000

    added the swap prototype

--- sort.h
+++ sort.h
@@ -21,5 +21,6 @@ typedef struct listint_s
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
+void swap(listint_t **list, listint_t *node1, listint_t *node2);
 
 #endif
