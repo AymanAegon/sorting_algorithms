b3e02998dd7c9617bd411e9a5a9930422a539140
ODSEFIANI <92064461+ODSEFIANI@users.noreply.github.com>
2023-07-21 17:21:26 +0100

    Update 1-insertion_sort_list.c

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -29,3 +29,25 @@ void insertion_sort_list(listint_t **list)
 		head = head->next;
 	}
 }
+/**
+ *swap - swap two nodes*
+ *
+ *@list: The array to be sorted
+ *@temp: The temp node to be sorted
+ *@head: The head of the list
+ */
+void swap(listint_t **list, listint_t *temp, listint_t *head)
+{
+	if (temp->prev)
+		temp->prev->next = head;
+	if (head->next)
+		head->next->prev = temp;
+
+	temp->next = head->next;
+	head->prev = temp->prev;
+	temp->prev = head;
+	head->next = temp;
+
+	if (!head->prev)
+		*list = head;
+}
